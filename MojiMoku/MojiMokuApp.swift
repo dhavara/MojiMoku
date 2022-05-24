e4cabecd0165bfb32ff9b3f878771f6d2408f694
Dhava Rachmana Aziz <dhava.rachmana.aziz@gmail.com>
2022-05-24 08:57:41 +0700

    first commit

---
+++ MojiMoku/MojiMokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiMokuApp.swift
+//  MojiMoku
+//
+//  Created by Macbook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MojiMokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
